0f86d79726caccf9da7b3d994e39c8184b6621cc
nsbernstein52 <nsbernstein52@gmail.com>
2020-01-14 10:20:08 -0800

    Code sumArray recursively

--- src/sumArray.js
+++ src/sumArray.js
@@ -18,4 +18,43 @@
 
 var sumArray = function(arr) {
   // Your code here
+  // I: array of numbers
+  // O: sum of the numbers
+  // Assumptions/constraints
+  //   a) Data is good
+  //   b) Must be solved recursively
+
+  // test for not an array: return "Not an array"
+  if (!Array.isArray(arr)) {
+    return "Not an array"
+  } else if (arr.length === 0) {
+  // test for empty array: return "Empty array"
+    return "Empty array"
+  } else if (arr.length === 1) {
+  // base case: length of array === 1, return that number
+    return arr[0];
+  } else {
+    // recursive case: length of array > 1, call recursively
+    return arr.pop() + sumArray(arr)   
+  }
 };
+
+// sumArray: data and tests
+
+let numArr = "string";
+console.log("string=>Not an array: ", sumArray(numArr));
+
+numArr = [];
+console.log("[]=>Empty array: ", sumArray(numArr));
+
+numArr = [10, 20];
+console.log("[10, 20]:30 ", sumArray(numArr));
+
+numArr = [2, 4, 6, 8];
+console.log("[2,4,6,8]:20 ", sumArray(numArr));
+
+numArr = [1, 2, 3, 4, 5];
+console.log("[1 ... 5]:15 ", sumArray(numArr));
+
+numArr = [1, -2, 3, -4, 5, -6];
+console.log("[1,-2,...-6]:-3 ", sumArray(numArr));
