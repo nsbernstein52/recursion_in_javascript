6afce9106ea46c8f404ad4a8c3e868501c20bd63
nsbernstein52 <nsbernstein52@gmail.com>
2020-01-15 09:13:57 -0800

    Code getElementsByClassName.js recursively

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,5 +7,48 @@
 
 
 var getElementsByClassName = function(className) {
-  // Your code here
-};
+  // I: className, string: search for className
+  // O: list of the nodes that with class name of className
+  // Assumptions/constraints:
+  //   Do not use existing function of the same name
+  //   Use:  `document.body`, `element.childNodes`, and `element.classList`
+  //   Permitted for use:  underscore functions
+  
+  // return variable: nodeList
+  var nodeList = [];
+  // alias for document.body
+  let bodyElement = document.body;
+
+  var elementsContainingClass = function(element) {
+    // I: element (node element) to be inspected for className
+    // O: element(s) that contain className
+    // Assumptions/constraints:
+    //   This is the recursive function
+    //   Used arr.forEach instead of possible _.each
+
+    // base case: the element contains the class className
+    if (element.classList && element.classList.contains(className)) {
+      // add it to the result array (within scope from parent function)
+      nodeList.push(element);
+    }
+
+    // recursive case: the element contains child nodes
+    if (element.childNodes) {
+      // loop through each child node
+      element.childNodes.forEach(function(item) {
+        // recursively call through all child nodes
+        elementsContainingClass(item);
+      });
+    }
+  }
+
+  // call (initiate) the recursive function from within getElementsByClassName
+  elementsContainingClass(bodyElement);
+
+  return nodeList;
+};  
+
+// getElementsByClassName: data and test cases
+let elementsByClassName = getElementsByClassName("targetClassName");
+// console.log for testing
+console.log("elementsByClassName: ", elementsByClassName);
